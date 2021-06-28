ed48ea4d77c289f2d5e765b98290186c951ab8f6
Netanel Sedgi <59725781+nsedgi@users.noreply.github.com>
2021-06-28 15:16:41 +0300

    Create BossCamera.cs

---
+++ Assets/Scripts/Camera/BossCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossCamera : MonoBehaviour
+{
+    [SerializeField] private CameraController cameraController;
+    [SerializeField] private float tweakX;
+    [SerializeField] private float tweakY;
+    [SerializeField] private float zoom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            cameraController.setBossStage(true);
+            cameraController.TraverseToBossStage(this.transform,tweakX,tweakY,zoom);
+        }
+    }
+}
