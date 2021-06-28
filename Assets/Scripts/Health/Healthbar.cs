42388289b26ca1ea859a6152b6cd23b968b008f8
Netanel Sedgi <59725781+nsedgi@users.noreply.github.com>
2021-06-28 15:20:58 +0300

    Create Healthbar.cs

---
+++ Assets/Scripts/Health/Healthbar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    [SerializeField] private Health playerHealth;
+    [SerializeField] private Image healthbarTotal;
+    [SerializeField] private Image healthbarCurrent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthbarCurrent.fillAmount = playerHealth.currentHealth/playerHealth.startingHealth;
+    }
+}
