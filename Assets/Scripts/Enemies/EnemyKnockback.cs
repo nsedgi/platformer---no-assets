2cc3b782645cca86c7aed55ef9d6b69e3b691028
Netanel Sedgi <59725781+nsedgi@users.noreply.github.com>
2021-06-28 15:21:41 +0300

    Create EnemyKnockback.cs

---
+++ Assets/Scripts/Enemies/EnemyKnockback.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyKnockback : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D body;
+    [SerializeField] private Health damageCheck;
+    [SerializeField] private EnemyBehavior enemyMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (body.velocity.x == 0 && damageCheck.isHit == false)
+        {
+
+            enemyMovement.isHurt = false;
+        }
+
+    }
+    public void InitiateKnockback(float direction, float knockbackForce, float upwardKnockbackFactor)
+    {
+        body.velocity = Vector2.zero;
+        enemyMovement.isHurt = true;
+        body.velocity = new Vector2(direction * knockbackForce, body.velocity.y + upwardKnockbackFactor);
+
+    }
+}
