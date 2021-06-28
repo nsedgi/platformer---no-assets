f4ce82a931e16dc9c178a7a3106e793b06d6b474
Netanel Sedgi <59725781+nsedgi@users.noreply.github.com>
2021-06-28 15:22:17 +0300

    Create PlayerAttack.cs

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    private Animator animator;
+    private PlayerMovement playerMovement;
+    private float cooldownTimer = Mathf.Infinity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl) && cooldownTimer>attackCooldown && playerMovement.canAttack())
+            Attack();
+        cooldownTimer += Time.deltaTime;
+    }
+
+    private void Attack()
+    {
+        animator.SetTrigger("attack1");
+        cooldownTimer = 0;
+    }
+}
