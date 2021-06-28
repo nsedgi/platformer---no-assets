8d15e1f371414a0e2966adccecb428a12d8c9a4a
Netanel Sedgi <59725781+nsedgi@users.noreply.github.com>
2021-06-28 15:21:26 +0300

    Create Knockback.cs

---
+++ Assets/Scripts/Knockback.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knockback : MonoBehaviour
+{
+    [SerializeField] private Health damageCheck;
+    [SerializeField] private Rigidbody2D body;
+    [SerializeField] private PlayerMovement playerMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (body.velocity.x == 0 && damageCheck.isHit == false)
+        {
+
+            playerMovement.enabled = true;
+        }
+    }
+
+    public void InitiateKnockback(float direction, float knockbackForce, float upwardKnockbackFactor)
+    {
+        playerMovement.enabled = false;
+        body.velocity = new Vector2(direction*knockbackForce, body.velocity.y+upwardKnockbackFactor);
+        damageCheck.isHit = false;
+    }
+}
